76e40441081e154986bfe2c0f8d162e503a57d9f
Rashid M Vayalil <rashiidmv@gmail.com>
2017-05-28 23:47:04 +0530

    Initial Setup : Infra project, prism libraries added

--- FriendMain/App.xaml.cs
+++ FriendMain/App.xaml.cs
@@ -13,5 +13,12 @@ namespace FriendMain
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Bootstrapper bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
---
+++ FriendMain/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace FriendMain
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Window mainWindow = (Window)Shell;
+            mainWindow.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            App.Current.MainWindow = mainWindow;
+            App.Current.MainWindow.Show();
+        }
+
+    }
+}
